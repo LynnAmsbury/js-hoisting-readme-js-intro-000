0a9d7e533a70732f7491273ead53174c6212ce0c
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-10 17:52:48 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -11,11 +11,14 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
-
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
+  thisIsCrazy();
+
+  // var thisIsCrazy = function (){
+  //   console.log("hey!!!")
+  // }
 }
 
 function sayMyName() {
